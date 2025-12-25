c22149b5a185e2d754d335ebe4f4465589e57c89
Kenzy Tarek <kenzy@Kenzys-MacBook-Pro.local>
2025-12-25 19:13:51 +0200

    details screen done

--- src/Components/Layout/ProjDetailsSection.jsx
+++ src/Components/Layout/ProjDetailsSection.jsx
@@ -5,23 +5,23 @@ import zooWebsite2 from "../../Assets/zoo website 2.png";
 import { supabase } from "../../Supabase";
 
 const ProjDetailsSection = () => {
+  const [Project, setProject] = useState([]);
 
-  const [Project , setProject] = useState([
-    { title: "2" }
-  ]);
+  useEffect(() => {
+    async function callGetAPI() {
+      const { data, error } = await supabase
+        .from("edit_screen")
+        .select("*");     
 
-  useEffect(()=>{
-    async function callGetAPI(){
-      const res = await supabase
-        .from("Project_details")
-        .select("*")
-        .eq("id", 2);   // <-- only get item with id = 2
-
-      setProject(res.data);
+      if (!error) {
+        setProject(data || []);
+      } else {
+        console.error(error);
+      }
     }
 
     callGetAPI();
-  },[])
+  }, []);
 
   const handleClick = () => {
     console.log("White button clicked!");
@@ -29,16 +29,15 @@ const ProjDetailsSection = () => {
 
   return (
     <div className="secounddivdeails">
-
-      {/* Use index 0 ALWAYS */}
       <h1 className="gizazoofonth1sec2">
-        {Project[0].title}
+        {Project[0]?.Title2}
       </h1>
 
       <img src={zooWebsite2} alt="Giza Zoo Website 2" />
 
       <h3 className="deatilsfont4">
-          {Project[0].description} </h3>
+        {Project[0]?.description2}
+      </h3>
 
       <ButtonDetailsWhite text="View Details" onClick={handleClick} />
     </div>
